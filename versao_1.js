2654156d04d4fae652a5d9ec2886cadc3b8e6aa9
Thiago Rosa <thiagorcon@hotmail.com>
2023-09-24 14:58:30 -0300

    Blackjack versão 1.0

--- versao_1.js
+++ versao_1.js
@@ -15,33 +15,33 @@ console.log('Boas vindas ao jogo de BlackJack!')
 let confirmacao = confirm("Quer iniciar uma nova rodada? \n (ok) - Para sim \n (cancelar) - Para não")
 
 if (confirmacao === true) {
-  alert('Oba, vamos jogar, abra o console!')
+     alert('Oba, vamos jogar, abra o console!')
 
-  let usuario1 = comprarCarta()
-  let usuario2 = comprarCarta()
-  let cpu1 = comprarCarta()
-  let cpu2 = comprarCarta()
-
-
-  //console.log(`${usuario1.valor} e ${usuario1.texto}`);
-  //console.log(`${usuario2.valor} e ${usuario2.texto}`);
-  //console.log(`${usuario2.valor} e ${usuario2.texto}`);
-  //console.log(`${cpu1.valor} e ${cpu1.texto}`);
-  //console.log(`${cpu2.valor} e ${cpu2.texto}`);
-
-  let somaUsuario = usuario1.valor + usuario2.valor
-  let somaCpu = cpu1.valor + cpu2.valor
-  console.log(`usuário cartas : ${usuario1.valor} ${usuario1.texto} e ${usuario2.valor} ${usuario2.texto} = ${somaUsuario}`);
-  console.log(`computador cartas : ${cpu1.valor} ${cpu1.texto} e ${cpu2.valor} ${cpu2.texto} = ${somaCpu}`);
-
-  if (somaUsuario > somaCpu) {
-    console.log(`O usuario venceu com ${somaUsuario} pontos`);
-  } else {
-    console.log(`O computador venceu com ${somaCpu} pontos`);
-  }
+     let usuario1 = comprarCarta()
+     let usuario2 = comprarCarta()
+     let cpu1 = comprarCarta()
+     let cpu2 = comprarCarta()
 
 
+     //console.log(`${usuario1.valor} e ${usuario1.texto}`);
+     //console.log(`${usuario2.valor} e ${usuario2.texto}`);
+     //console.log(`${usuario2.valor} e ${usuario2.texto}`);
+     //console.log(`${cpu1.valor} e ${cpu1.texto}`);
+     //console.log(`${cpu2.valor} e ${cpu2.texto}`);
+
+     let somaUsuario = usuario1.valor + usuario2.valor
+     let somaCpu = cpu1.valor + cpu2.valor
+     console.log(`usuário cartas : ${usuario1.valor} ${usuario1.texto} e ${usuario2.valor} ${usuario2.texto} = ${somaUsuario}`);
+     console.log(`computador cartas : ${cpu1.valor} ${cpu1.texto} e ${cpu2.valor} ${cpu2.texto} = ${somaCpu}`);
+
+     if (somaUsuario > somaCpu) {
+          console.log(`O usuario venceu com ${somaUsuario} pontos`);
+     } else if (somaUsuario < somaCpu) {
+          console.log(`O computador venceu com ${somaCpu} pontos`);
+     } else {
+          console.log(`EMPATE - O fez ${somaUsuario} pontos e o computador fez ${somaCpu}`);
+     }
 
 } else if (confirmacao === false) {
-  console.log('O jogo acabou!')
+     console.log('O jogo acabou!')
 }
